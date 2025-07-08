14386e57c60df65269ddc6c591b68e509a9a843d
snehccurry <chensukhgaur444@gmail.com>
2025-07-08 13:37:37 +0530

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8e6bdd45a243e8d6a03f1cbd70e74c96
 "canvaskit/skwasm_heavy.wasm": "5d7d4249bb9165bd1fcfe02d801f0e5c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3bc5c92b1047f60e01aed65c5249f29d",
+"flutter_bootstrap.js": "0594e6ab505de6266e6d0229b86aeeca",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
